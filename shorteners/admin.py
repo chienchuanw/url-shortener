e6e87bcf807cf591157de7e662f74ce9c8f9927b
Chien Chuan Wang <chien@chienchuan.com>
2024-06-26 12:17:45 +0800

    Created model 'ShortURL' and registered it

--- shorteners/admin.py
+++ shorteners/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ShortURL
 
-# Register your models here.
+
+class ShortURLAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at",)
+    list_display = (
+        "original_url",
+        "short_url",
+        "created_at",
+    )
+    search_fields = (
+        "original_url",
+        "short_url",
+    )
+
+
+admin.site.register(ShortURL, ShortURLAdmin)
--- shorteners/models.py
+++ shorteners/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ShortURL(models.Model):
+    original_url = models.URLField(
+        help_text="Enter a valid URL address", verbose_name="Original URL"
+    )
+    short_url = models.URLField(
+        max_length=100, unique=True, blank=True, verbose_name="Short URL"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Time")
+
+    class Meta:
+        verbose_name = "Short URL"
+        verbose_name_plural = "Short URLs"
+
+    def __str__(self):
+        return self.short_url
